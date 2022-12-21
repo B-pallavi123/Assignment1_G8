9940f8efd90b69c2163a187e4f9d9991d0f6f3b2
B-pallavi123 <118979872+B-pallavi123@users.noreply.github.com>
2022-12-21 17:21:57 +0530

    commit

---
+++ Scalability/src/com/prime/Prime.java
@@ -0,0 +1,20 @@
+package com.prime;
+
+public class Prime {
+
+	public static void main(String srgs[]) {
+		int n = 11, count = 0;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				count = count + 1;
+			}
+		}
+		if (count == 0) {
+			System.out.println("Given number is a prime number");
+		} else {
+			System.out.println("Given number is not a prime number");
+		}
+
+	}
+
+}
