b8c05d927dec2852d3d0b043be3ba044ac6aefb9
B-pallavi123 <118979872+B-pallavi123@users.noreply.github.com>
2023-01-03 08:31:37 +0530

    commit3

--- ExceptionHandling/src/com/javaExceptionExample/ExceptionHandling.java
+++ ExceptionHandling/src/com/javaExceptionExample/ExceptionHandling.java
@@ -5,30 +5,27 @@ import java.util.Scanner;
 public class ExceptionHandling {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("enter a number1:");
-		Scanner sc=new Scanner(System.in);
-		int a=sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
 		System.out.println("enter a number2");
-		Scanner scn=new Scanner(System.in);
-		int b=sc.nextInt();
-		int div = 0,mul=0;
+		Scanner scn = new Scanner(System.in);
+		int b = sc.nextInt();
+		int div = 0, mul = 0;
 		try {
-			div=a/b;
-			mul=a*b;
-		}
-		catch(ArithmeticException e){
+			div = a / b;
+			mul = a * b;
+		} catch (ArithmeticException e) {
 			System.out.println("divide by zero");
-			
-		}
-		catch(Exception e) {
+
+		} catch (Exception e) {
 			System.out.println("i am an exception");
-		}
-		finally {
+		} finally {
 			System.out.println("i am always executed");
 
-	}
-		System.out.println("product is "+mul);
+		}
+		System.out.println("product is " + mul);
 
 		// TODO Auto-generated method stub
 
