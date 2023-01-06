07296d1c30d4e24ee30b473030aba9eb3ecfcfc5
B-pallavi123 <118979872+B-pallavi123@users.noreply.github.com>
2023-01-06 13:31:08 +0530

    commit7

--- ExceptionHandling/src/com/javaExceptionExample/Palindrome.java
+++ ExceptionHandling/src/com/javaExceptionExample/Palindrome.java
@@ -15,24 +15,26 @@ public class Palindrome {
 		if (n == reverse && n!=0) {
 			System.out.println("Given number is palindrome");
 		}
+		else if(a==0) {
+			throw new Exception("enter a number greater than zero");
+			
+		}
 
 		else {
 			System.out.println("Given number is not a palindrome");
 		}
 	}
 
-	public static void main(String[] args) {
-		try {
+	public static void main(String[] args) throws Exception {
+		
 			System.out.println("enter atleat 3 digit number: ");
 			Scanner sc = new Scanner(System.in);
 			int num = sc.nextInt();
 			palindrome(num);
 
-		}
+		
 
-		catch (Exception e) {
-			System.out.println(e + "enter a valid number not a string");
-		}
+	
 	}
 
 }
